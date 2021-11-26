ff1e645fe4cb54e5804047521df98cd7e60f55fe
Nitin Sawant <sawantnitin55@gmail.com>
2021-11-26 15:21:10 +0530

    updated driver path

--- shopify-ui-automation/src/test/java/base/BaseClass.java
+++ shopify-ui-automation/src/test/java/base/BaseClass.java
@@ -9,7 +9,7 @@ public class BaseClass {
 	protected WebDriver driver;
 	
 	public void initialization() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Driver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\java\\driver\\chromedriver.exe");
 		driver = new ChromeDriver();	
 		driver.manage().window().maximize(); //maximize window
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
